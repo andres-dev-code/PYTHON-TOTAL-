4e329949f5401c477c663e6712115bae92024c3b
andres-dev-code <andresdev06@gmail.com>
2025-12-07 19:06:42 -0300

    Delete Dia_6/Dia_6 directory

--- Dia_6/Dia_6/Escribir_Archivos.py
+++
@@ -1,6 +0,0 @@
-archivo = open('Texto','w')
-
-palabras = ['Hola ','Mundo ','como ','estan? ']
-for p in palabras:
-    archivo.write(p)
-archivo.close()
--- Dia_6/Dia_6/Limpiar_la_consola.py
+++
@@ -1,8 +0,0 @@
-from os import system
-
-nombre = input('Dime tu nombre:')
-edad = input('Dime tu edad:')
-system('cls')
-print(f'tienes {edad} y tu nombre es {nombre}')
-
-
